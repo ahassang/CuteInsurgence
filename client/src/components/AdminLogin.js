83e4ab03e9ac2efe3b1310c1b70d3c18a8c965b3
Antonio Hassang <globaltailwind@protonmail.com>
2021-03-30 09:36:09 -0300

    I am commiting, I am being prompted to

--- client/src/components/AdminLogin.js
+++ client/src/components/AdminLogin.js
@@ -1,10 +1,28 @@
-import React from 'react';
+import React, {useState} from 'react';
 import axios from 'axios';
 import {Link} from '@reach/router';
 import {Form} from 'react-bootstrap';
 
-const AdminLogin = () =>{
-    return(
+const AdminLogin = (props) => {
+        const [name, setName]= useState('');
+        const [password, setPassword]= useState('');
+        const [errMessage, setErrMessage] = useState('');
+    
+        const login = (e) => {
+            e.preventDefault();
+            axios.post('http://localhost:8000/api/admin/login', {
+                name: name,
+                password: password})
+            .then(response => {
+                console.log("login data", response.data);
+                Nav(`/admin/home`)
+            })
+            .catch(error => {
+                console.log("problem with login.js",error);
+                setErrMessage(error.response.data.msg);
+        })
+    };
+    return (
         <div>
             <h1> AdminLogin.js </h1>
             <p><Link to= '/admin/home'>Admin Home</Link></p> 
